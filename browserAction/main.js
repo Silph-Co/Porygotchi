5bd284b23ed088642f6a19eff7a15de592004c71
April Sylph <april@sylph.tech>
2019-03-10 19:11:38 +0000

    clean main.js

--- browserAction/main.js
+++ browserAction/main.js
@@ -1,10 +1,9 @@
 const porygon = document.getElementById("porygon");
+
 function wiggle() {
 	if (porygon.getAttribute("src") === "images/static.png") {
 		porygon.setAttribute("src", "images/animated.png");
-		setTimeout(() => {
-			porygon.setAttribute("src", "images/static.png");
-		}, 600);
+		setTimeout(() => porygon.setAttribute("src", "images/static.png"), 600);
 	}
 }
 
