eceb3694433b4689f13ddff2957523230df2f7f0
April Sylph <april@sylph.tech>
2019-03-18 18:55:21 +0000

    Add background image
    comfy :)

--- browserAction/main.js
+++ browserAction/main.js
@@ -1,9 +1,9 @@
 const porygon = document.getElementById("porygon");
 
 function wiggle() {
-	if (porygon.getAttribute("src") === "images/static.png") {
-		porygon.setAttribute("src", "images/animated.png");
-		setTimeout(() => porygon.setAttribute("src", "images/static.png"), 600);
+	if (porygon.getAttribute("src") === "images/porygon/static.png") {
+		porygon.setAttribute("src", "images/porygon/animated.png");
+		setTimeout(() => porygon.setAttribute("src", "images/porygon/static.png"), 600);
 	}
 }
 
